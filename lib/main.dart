baadeed12d3e43cff767e4b89c379162c5a9b26a
Lohuama <limalorruama98@gmail.com>
2023-06-05 14:01:19 -0300

    add stars

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,16 @@ class MyApp extends StatelessWidget {
         ),
         body: ListView(
           children: [
-            Task('Aprender Flutter'),
-            Task("Andar de Bike"),
-            Task("Meditar"),
+            Task('Aprender Flutter',
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
+            Task("Andar de Bike",
+                'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg'),
+            Task("Meditar",
+                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
+            Task("Ler",
+                'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg'),
+            Task("Jogar",
+                'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg'),
           ],
         ),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
@@ -35,7 +42,8 @@ class MyApp extends StatelessWidget {
 
 class Task extends StatefulWidget {
   final String nome;
-  const Task(this.nome, {Key? key}) : super(key: key);
+  final String foto;
+  const Task(this.nome, this.foto, {Key? key}) : super(key: key);
 
   @override
   State<Task> createState() => _TaskState();
@@ -65,14 +73,53 @@ class _TaskState extends State<Task> {
                       color: Colors.black26,
                       width: 75,
                       height: 100,
-                    ),
-                    Container(
-                      width: 200,
-                      child: Text(
-                        widget.nome,
-                        style: TextStyle(
-                            fontSize: 24, overflow: TextOverflow.ellipsis),
+                      child: Image.network(
+                        widget.foto,
+                        fit: BoxFit.cover,
                       ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 200,
+                          child: Text(
+                            widget.nome,
+                            style: TextStyle(
+                                fontSize: 24, overflow: TextOverflow.ellipsis),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: Colors.blue,
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: Colors.blue,
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: Colors.blue,
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: Colors.blue[100],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: Colors.blue[100],
+                            ),
+                          ],
+                        )
+                      ],
                     ),
                     Container(
                       height: 52,
