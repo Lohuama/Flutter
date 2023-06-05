19c67befee3ac8c1265cdc1f8e17bd5008caf838
Lohuama <limalorruama98@gmail.com>
2023-06-05 12:49:07 -0300

    ListView e Padding

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,10 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Tarefas"),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: ListView(
           children: [
-            Task('Aprender Flutter'),
+            Task(
+                'Aprender Flutter'),
             Task("Andar de Bike"),
             Task("Meditar"),
           ],
@@ -39,30 +39,40 @@ class Task extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Stack(
-      children: [
-        Container(
-          color: Colors.blue,
-          height: 140,
-        ),
-        Container(
-          color: Colors.white,
-          height: 100,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                color: Colors.black26,
-                width: 75,
-                height: 100,
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+            child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.black26,
+                    width: 75,
+                    height: 100,
+                  ),
+                  Container(
+                    width: 200,
+                    child: Text(
+                      nome,
+                      style: TextStyle(
+                          fontSize: 24, overflow: TextOverflow.ellipsis),
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+                ],
               ),
-              Text(nome),
-              ElevatedButton(onPressed: () {}, child: Icon(Icons.arrow_drop_up))
-            ],
-          ),
-        ),
-      ],
-    ));
+            ),
+          ],
+        )));
   }
 }
